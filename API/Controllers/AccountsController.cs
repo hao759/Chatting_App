c6b6fdb8793f505e7f5d9b785d99bec2c45d516b
hao759 <qhao74154@gmail.com>
2023-12-21 21:26:25 +0700

    add LoginDTO

--- API/Controllers/AccountsController.cs
+++ API/Controllers/AccountsController.cs
@@ -1,6 +1,8 @@
 ﻿using API.Data;
+using API.DTO;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Security.Cryptography;
 using System.Text;
 
@@ -20,11 +22,25 @@ namespace API.Controllers
             var user = new AppUser
             {
                 Name = username,
-                PasswordHash=hmac.ComputeHash(Encoding.UTF8.GetBytes(password)),
-                PasswordSalt=hmac.Key
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password)),
+                PasswordSalt = hmac.Key
             };
             _context.appUsers.Add(user);
             await _context.SaveChangesAsync();
+            return Ok(user);
+        }
+
+        [HttpPost("login")] //api/control/regis
+        public async Task<ActionResult<AppUser>> Login(LoginDTO login)
+        {
+            var user = await _context.appUsers.FirstOrDefaultAsync(s => s.Name == login.Name);
+            if (user == null)
+                return Unauthorized();
+            using var hmac = new HMACSHA512(user.PasswordSalt);
+            var ComputeHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(login.password));
+            for (int i = 0; i < ComputeHash.Length; i++)
+                if (ComputeHash[i] != user.PasswordHash[i])
+                    return Unauthorized("Sai pass");
             return Ok(user);
         }
 
---
+++ API/DTO/LoginDTO.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.DTO
+{
+    public class LoginDTO
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string password { get; set; }
+    }
+}
