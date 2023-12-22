4d2f1451b7e0b6cd53bf0f666e45788964f70406
hao759 <qhao74154@gmail.com>
2023-12-22 22:30:00 +0700

    async pipe

--- clientApp/src/app/Component/nav/nav.component.ts
+++ clientApp/src/app/Component/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Observable, of } from 'rxjs';
 import { User } from 'src/app/_models/User';
 import { AccountService } from 'src/app/_service/account.service';
 
@@ -9,27 +10,28 @@ import { AccountService } from 'src/app/_service/account.service';
 })
 export class NavComponent {
   Model: any = {};
-  public LoggedIn = false
-  constructor(private accountService: AccountService) {
-    this.getCurrentUser()
+  // public LoggedIn = false
+  currentUser: Observable<User | null> = of(null)
+
+
+  constructor(public accountService: AccountService) {
+    // this.currentUser = this.accountService.currentUser
   }
 
   onSubmit() {
     console.log(this.Model)
     this.accountService.login(this.Model).subscribe(data => {
       console.log(data)
-      this.LoggedIn = true
     });
 
   }
   logout() {
-    this.LoggedIn = false
     this.accountService.logout()
   }
-  getCurrentUser() {
-    this.accountService.currentUser.subscribe({
-      next: User => this.LoggedIn = !!User,
-      error: error => console.log(error)
-    })
-  }
+  // getCurrentUser() {
+  //   this.accountService.currentUser.subscribe({
+  //     next: User => this.LoggedIn = !!User,
+  //     error: error => console.log(error)
+  //   })
+  // }
 }
