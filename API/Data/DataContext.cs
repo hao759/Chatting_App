675cd31cd64da62c45c3a46f8ce7038590679f50
hao759 <qhao74154@gmail.com>
2024-01-05 14:37:43 +0700

    add UserLike entity

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -10,6 +10,47 @@ namespace API.Data
         {
 
         }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            // builder.Entity<AppUser>()
+            //     .HasMany(ur => ur.UserRoles)
+            //     .WithOne(u => u.User)
+            //     .HasForeignKey(ur => ur.UserId)
+            //     .IsRequired();
+
+            // builder.Entity<AppRole>()
+            //     .HasMany(ur => ur.UserRoles)
+            //     .WithOne(u => u.Role)
+            //     .HasForeignKey(ur => ur.RoleId)
+            //     .IsRequired();
+
+            builder.Entity<UserLike>()
+                .HasKey(k => new { k.SourceUserId, k.TargetUserId });
+
+            builder.Entity<UserLike>()
+                .HasOne(s => s.SourceUser)
+                .WithMany(l => l.LikedUsers)
+                .HasForeignKey(s => s.SourceUserId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.Entity<UserLike>()
+                .HasOne(s => s.TargetUser)
+                .WithMany(l => l.LikedByUsers)
+                .HasForeignKey(s => s.TargetUserId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            // builder.Entity<Message>()
+            //         .HasOne(u => u.Recipient)
+            //         .WithMany(m => m.MessagesReceived)
+            //         .OnDelete(DeleteBehavior.Restrict);
+
+            // builder.Entity<Message>()
+            //     .HasOne(u => u.Sender)
+            //     .WithMany(m => m.MessagesSent)
+            //     .OnDelete(DeleteBehavior.Restrict);
+        }
         protected override void ConfigureConventions(ModelConfigurationBuilder builder)
         {
             base.ConfigureConventions(builder);
@@ -26,6 +67,7 @@ namespace API.Data
         }
 
         public DbSet<AppUser> appUsers { get; set; }
+        public DbSet<UserLike> userLikes { get; set; }
     }
 }
 
